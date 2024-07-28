1c27b44614744799353672b7024ebc8b50c42b93
Vitalii <vitaliti.yurchenko@gmail.com>
2024-07-28 14:57:27 +0300

    Add total and sum of numbers

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,13 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int sum = 0;
         //Ініціалізуємо та присвоюємо значення змінній лічильника
+        //Ініціалізуємо та присвоюємо значення змінній,
+        //яка буде рахувати суму чисел кожного рядку
         int count = 0;
+        int sum = 0;
 
         for (int number = 1; number <= 6; number++){
-            count++;
-            System.out.println(count + ") Num is " + number);
+            count++; //Нумеруємо кожен рядок через інкремент
+            sum += number; //Рахуємо суму чисел наростаючим підсумком
+            System.out.println(count + ") Num is " + number + ", sum is " + sum);
         }
+        System.out.println("------------------------\n" +
+                "Sum of numbers is " + sum);
     }
 }
