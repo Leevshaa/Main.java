18ab61e30b0442b7e394ac40d6e95d0640af6963
Vitalii <vitaliti.yurchenko@gmail.com>
2024-07-28 14:04:47 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        //Ініціалізуємо та присвоюємо значення змінній лічильника
+        int count = 0;
+
+        for (int number = 1; number <= 6; number++){
+            count++;
+            System.out.println(count + ") Num is " + number);
+        }
+    }
+}
